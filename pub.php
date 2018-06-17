4011382abf6776036cb0acdc721997bcf2a55d5d
HEX <kamtorn.ku@gmail.com>
2018-06-17 09:59:50 +0700

    ass

--- pub.php
+++ pub.php
@@ -37,7 +37,7 @@
      
     curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
  
-    //curl_setopt($ch, CURLOPT_USERPWD, "mJ7K4MfteC7p0dW:pp4gzMhCvJIqlxc66hKEvk46m");
+    curl_setopt($ch, CURLOPT_USERPWD, "ovlN1MiClOVtq9O:rdtuDSTPPVAJA7ryBDOTj8P6z");
      
     $response = curl_exec($ch);
     
@@ -45,7 +45,7 @@
       echo $response . "\r\n";
     return $response;
 }
-// $Topic = "NodeMCU1";
- //$lineMsg = "CHECK";
- //getMqttfromlineMsg($Topic,$lineMsg);
+ $Topic = "NodeMCU1";
+ $lineMsg = "CHECK";
+ getMqttfromlineMsg($Topic,$lineMsg);
 ?>
--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'xwn+HT7h/C6E9kcHQSeZ8YN4rY3EHdJdNIOtCUDOtWChZUTz6GfNlQoAEVSukGBd0S6pLaXwL/s/i62DoabJomYXsAl2kxa+wH3a+N2t83OvQW5AMKPNphegYkTjy72A2ScvK5xCNqBXej85i0HYmwdB04t89/1O/w1cDnyilFU';
+$access_token = 'xwn+HT7h/C6E9kcHQSeZ8YN4rY3EHdJdNIOtCUDOtWChZUTz6GfNlQoAEVSukGBd0S6pLaXwL/s/i62DoabJomYXsAl2kxa+wH3a+N2t83OvQW5AMKPNphegYkTjy72A2ScvK5xCNqBXej85i0HYmwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
