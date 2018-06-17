a494fc765a67daf7f7946741f7344f7dbb52c25e
HEX <kamtorn.ku@gmail.com>
2018-06-17 21:57:00 +0700

    msg fix

--- line.php
+++ line.php
@@ -3,18 +3,13 @@
 function send_LINE($msg,$reply_token){
  $access_token = 'LWJCoT6Vuz2x0pDhLtyW+wXxpJ+9M7Tx6P5K1bFroeStnMNnh2C46Tnd8H+wWDBg0S6pLaXwL/s/i62DoabJomYXsAl2kxa+wH3a+N2t83O76hO76yDdmyLyFTpQFRW6SIWuCbkkRFwZVhH3wxedPwdB04t89/1O/w1cDnyilFU='; 
 
-  $messages = [
-        'type' => 'text',
-        'text' => $msg
-        //'text' => $text
-      ];
 
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/reply';
       $data = [
 
         'to' => $reply_token,
-        'messages' => [$messages],
+        'messages' => [['type' => 'text', 'text' => $msg]]
       ];
       $post = json_encode($data);
       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
